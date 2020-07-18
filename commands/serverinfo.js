94d0a3ecd708f781828420e93f1bad080ce2e86d
sp00p <46919543+sp00p@users.noreply.github.com>
2020-07-18 09:25:41 -0700

    fix member presences hopefully
    i'll be adding the offline counter later

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,7 +4,13 @@ module.exports = {
   name: "serverinfo",
   description: "sends infomation about the guild you\'re in",
   arguments: "none",
-  execute(message, guild, serverIcon, online, idle, dnd, offline) {
+  execute(message, guild, serverIcon) {
+    
+    message.guild.members.fetch().then(fetchedMembers => {
+      const onlineMembers = fetchedMembers.filter(member => member.presence.status === "online");
+      const idleMembers = fetchedMembers.filter(member => member.presence.status === "idle");
+      const dndMembers = fetchedMembers.filter(member => member.presence.status === "dnd");
+    })
 
     const serverInfoEmbed = new Discord.MessageEmbed()
       .setColor("0x0099ff")
@@ -17,7 +23,7 @@ module.exports = {
         {name: "**Nitro Tier**", value: guild.premiumTier, inline: true},
         {name: "**Nitro Boosts**", value: guild.premiumSubscriptionCount, inline: true},
         {name: "**Verification Level**", value: guild.verificationLevel},
-        {name: "**Member Prescences**", value: `🟢 Online ${online}\n 🟠 Idle ${idle}\n 🔴 Do Not Disturb ${dnd}\n ⚫ Offline ${offline}`, inline: true}
+        {name: "**Member Prescences**", value: `🟢 Online ${onlineMembers}\n 🟠 Idle ${idleMembers}\n 🔴 Do Not Disturb ${dndMembers}`, inline: true}
 
       )
       .setFooter("Kukita Bot", "https://cdn.discordapp.com/attachments/731996957051977859/733879306283122758/kukita.png");

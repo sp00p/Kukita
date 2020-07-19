11c3cf841e9c33cf2b3e0b0dbeeeca80107abd15
Sean <seancornell11@gmail.com>
2020-07-19 00:20:53 -0700

    formatting issue

--- commands/inviteme.js
+++ commands/inviteme.js
@@ -3,6 +3,6 @@ module.exports = {
   description: "sends an oath link so you can invite me to your server",
   arguments: "none",
   execute(message) {
-    message.author.send("As you requested, here's my [OAuth link!](https://discord.com/api/oauth2/authorize?client_id=714758633702948905&permissions=8&scope=bot)")
+    message.author.send("As you requested, here's my OAuth link! https://discord.com/api/oauth2/authorize?client_id=714758633702948905&permissions=8&scope=bot")
   }
 }

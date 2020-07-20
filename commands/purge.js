8db10de781c672c87c312d0c89097cac45e89ff7
Sean <seancornell11@gmail.com>
2020-07-19 23:00:53 -0700

    fix purge

--- commands/purge.js
+++ commands/purge.js
@@ -2,16 +2,16 @@ module.exports = {
   name: "purge",
   description: "deletes a set amount of messages",
   arguments: "<number of messages>",
-  execute(message, number) {
+  execute(message, args) {
       if (message.member.hasPermission("MANAGE_MESSAGES", "ADMINISTRATOR")) {
 
-      let num = parseInt(number);
+      let num = parseInt(args[1]);
 
       if (num > 100) return message.reply("Please enter a number less than or equal to 100");
 
       message.channel.messages.fetch({ limit: num}).then(messages => {
         message.channel.bulkDelete(messages).then(deleted => {
-          message.reply(`${deleted.size} messages have been successfully deleted!`).then(message => message.delete(5000));
+          message.reply(`${deleted.size} messages have been successfully deleted!`).then(message => message.delete());
         })
       })
     } else {

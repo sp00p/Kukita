48ab1aef856bd3a7e1bf55d69e1292e8e0358b39
Sean <seancornell11@gmail.com>
2020-07-20 17:16:37 -0700

    add serveremojis command

---
+++ commands/utility/serveremojis.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports.run =  async (bot, message, args) => {
+
+  let Emojis = "";
+  let EmojisAnimated = "";
+  let EmojiCount = 0;
+  let Animated = 0;
+  let OverallEmojis = 0;
+  function Emoji(id) {
+    return bot.emojis.cache.get(id).toString()
+  }
+  message.guild.emojis.cache.forEach(emoji => {
+    OverallEmojis++;
+    if (emoji.animated) {
+      Animated++;
+      EmojisAnimated += Emoji(emoji.id)
+    } else {
+      EmojiCount++;
+      Emojis += Emoji(emoji.id)
+    }
+  })
+  let emojiEmbed = new MessageEmbed()
+    .setTitle(`**Emojis in ${message.guild.name}**`)
+    .setDescription(`**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emojis [${OverallEmojis}]**`)
+    message.channel.send(emojiEmbed)
+}
+
+
+module.exports.help = {
+  name: "serveremojis",
+  description: "displays all emojis available in the guild",
+  arguments: "none",
+  category: "Utility",
+  aliases: ["serveremojis", "emojis"]
+}

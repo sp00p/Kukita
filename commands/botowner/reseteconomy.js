8d3eb0bdbb332cd7e15841b7004dbcf08db61b25
Sean <seancornell11@gmail.com>
2020-07-23 14:43:37 -0700

    Update reseteconomy.js

--- commands/botowner/reseteconomy.js
+++ commands/botowner/reseteconomy.js
@@ -16,14 +16,14 @@ module.exports.run = async (bot, message, args) => {
     if (err) console.log(err);
 
     if(!res) {
-      moneyEmbed.setColor("#fc0404");
-      moneyEmbed.addField("❌ Error", "Data could not be deleted!");
-      moneyEmbed.setDescription("lol they dont have money xdddd")
+      moneyDeleteEmbed.setColor("#fc0404");
+      moneyDeleteEmbed.addField("❌ Error", "Data could not be deleted!");
+      moneyDeleteEmbed.setDescription("lol they dont have money xdddd")
       message.channel.send(moneyDeleteEmbed);
     } else {
       moneyDeleteEmbed.setColor("#00FF00");
       moneyDeleteEmbed.addField(`${guild}'s data expuged!`)
-      moneyEmbed.setDescription("lol they money gone")
+      moneyDeleteEmbed.setDescription("lol they money gone")
       message.channel.send(moneyDeleteEmbed);
 
     }

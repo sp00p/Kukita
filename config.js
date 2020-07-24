0c3483ff33ff2c4047efc0ac09a5f7b7cbe9047a
Sean <seancornell11@gmail.com>
2020-07-23 22:58:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   token: "NzE0NzU4NjMzNzAyOTQ4OTA1.XxYQQg.FnzQPJsRlNriAWXRCiaUf-95d1w",
   prefix: ".",
   owners: ["257663701505671169"],
-  betatesters: ["257663701505671169"],
+  betatesters: ["257663701505671169", "411250289782292490"],
 };

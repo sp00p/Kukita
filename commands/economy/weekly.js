4848e473c1389e16152c29e904e2a105a15c707f
Sean <seancornell11@gmail.com>
2020-07-28 17:54:47 -0700

    fix daily pls

--- commands/economy/weekly.js
+++ commands/economy/weekly.js
@@ -27,14 +27,14 @@ module.exports.run = async (bot, message, args) => {
 
         return message.channel.send(noAccountEmbed)
 
-    } else if (data.weeklyCooldown - Date.now() >= 6.048e+8){
+    } else if (data.weeklyCooldown >= Date.now()){
 
           data.money = data.money + 700;
-          data.weeklyCooldown = Date.now()
+          data.weeklyCooldown = Date.now() + 6.048e+8
           data.save()
           return message.channel.send(weeklyWorkEmbed)
 
-    } else if (data.weeklyCooldown - Date.now() < 6.048e+8) {
+    } else if (data.weeklyCooldown < Date.now()) {
         //console.log('cooldown more than date')
 
         var remaining = humanizeDuration(data.weeklyCooldown - Date.now(), { conjunction: " and ", units: ["d", "h", "m", "s"], round: true});

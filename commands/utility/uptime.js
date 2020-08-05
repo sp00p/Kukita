ae0f1f556575bc44847d8ce834ae963e970ae3e5
Sean <seancornell11@gmail.com>
2020-08-05 00:23:07 -0700

    Update uptime.js

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -2,12 +2,12 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports.run = (bot, message,args) => {
   let totalSeconds = (bot.uptime / 1000);
-  let days = Math.floor(totalSeconds / 86400);
+  let days = Math.floor(totalSeconds / 86400).toString();
   totalSeconds %= 86400;
-  let hours = Math.floor(totalSeconds / 3600);
+  let hours = Math.floor(totalSeconds / 3600).toString();
   totalSeconds %= 3600;
-  let minutes = Math.floor(totalSeconds / 60);
-  let seconds = Math.floor(totalSeconds % 60)
+  let minutes = Math.floor(totalSeconds / 60).toString();
+  let seconds = Math.floor(totalSeconds % 60).toString();
   const uptimeEmbed = new MessageEmbed()
     .setColor("#0x0099ff")
     .setAuthor("Kukita#6512", "https://cdn.discordapp.com/attachments/731996957051977859/733879306283122758/kukita.png")
